47dbc0bd5ca7f8cc14cce5b6d2a288014b8ad7d4
User <User@LAPTOP-TBL2JDIU>
2020-04-29 10:51:16 -0600

    Prueba de repositorio, Diego Samayoa.

---
+++ Examen parcial 2/src/examen/parcial/pkg2/ExamenParcial2.java
@@ -0,0 +1,14 @@
+
+package examen.parcial.pkg2;
+
+
+public class ExamenParcial2 {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Diego Samayoa");
+        System.out.println("Hola Mundo)");
+    }
+    
+}
